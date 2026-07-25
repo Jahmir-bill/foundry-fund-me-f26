8ab51f625cf9e3f86a82333984f27d09bd1f30ad
jahmir-bill <kingvictor296@gmail.com>
2026-07-25 19:10:59 +0100

    fix DevOpsTools import casing for case-sensitive CI

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -6,7 +6,7 @@
 pragma solidity ^0.8.18;
 
 import {Script, console} from "forge-std/Script.sol";
-import {DevOpsTools} from "foundry-devops/src/DevopsTools.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract FundFundMe is Script {
